9bdabfd55273e6fe527463d40d8d1b91364c285b
Lenpka <you@example.com>
2025-08-08 23:05:46 +0300

    D

---
+++ join/join.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+std::string Join(std::vector<std::string> tokens, char delimiter)
+{
+	const size_t size = tokens.size();
+	std::string ans = "";
+	if (size == 0)
+	{
+		return "";
+	}
+	else if (size == 1)
+	{
+		return tokens[0];
+	}
+		for (size_t i = 0; i < size-1; i++)
+		{
+
+			ans.append(tokens[i] + delimiter);
+		}
+		ans.append(tokens[size-1]);
+		
+	return ans;
+}
+
+int main()
+{
+	std::cout << Join({"z", "o", "v"}, '_');
+}
