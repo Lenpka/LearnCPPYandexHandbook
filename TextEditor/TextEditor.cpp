64df1024547b1f554126f65d5377357b3b6a94ed
Lenpka <you@example.com>
2025-08-16 16:55:00 +0300

    Version 1 before advance and splice

--- TextEditor/TextEditor.cpp
+++ TextEditor/TextEditor.cpp
@@ -1,84 +1,208 @@
 ﻿#include <iostream>
 #include <list>
-#include <vector>
 #include <string>
-#include <iterator>
-
-
+#include <vector>
 int main()
 {
-	std::string input;
-	std::list <std::string> output;
+	std::string inp;
+	std::list<std::string> out;
 
-	while (std::getline(std::cin, input))
+	while (std::getline(std::cin, inp))
 	{
-		
-		if (input == "")
+		out.push_back(inp);
+		if (inp.empty())
 		{
 			break;
 		}
-		output.push_back(input);
 
 	}
-	std::list<std::string> ::iterator state = output.begin();
-	std::string copy;
-	while (std::getline(std::cin, input))
+	std::string copy = "";
+	std::list <std::string> ::iterator EndShiftState;
+	std::list <std::string> ::iterator StartShiftState;
+	std::vector <std::string> PasteBoofer;
+	std::list<std::string>::iterator state = out.begin();
+
+	bool ShiftState = 0;
+	int counter_up = 0;
+	int counter_down = 0;
+	bool Special = 0;
+	std::vector <std::string>ShiftBoofer = {};
+
+	while (std::getline(std::cin, inp))
 	{
-		if ((state != std::prev(output.end())))
-		{
-			if (input == "Down")
-			{
-				++state;
-			}
-		}
 
-		if (state != output.begin())
-		{
-			if (input == "Up")
-			{
-				--state;
-			}
-			if (input == "Ctrl+X")
-			{
-				copy = *state;
-				state = output.erase(state);	
-			}
-		}
-		
-		if (state == output.begin())
-		{
-			if (input == "Ctrl+X")
-			{
-				copy = *state;
-				state = output.erase(state);
-			}
-		}
-
-
-		if (input == "Ctrl+V")
-		{
-			++state;
-			output.insert(--state, copy);
-		}
-		if (input == "")
+		//if input not empty
+		if (inp.empty())
 		{
 			break;
 		}
+
+	
+		
+		//UP AND DOWN
+
+		if (out.size() > 0)
+		{
+			if (inp == "Shift")
+			{
+				ShiftState = !ShiftState;
+				StartShiftState = state;
+			}
+
+			if (inp == "Down")
+			{
+
+				if (state != std::prev(out.end())and !ShiftState)
+				{
+					++state;
+				}
+
+				else if ((ShiftState and state != std::prev(out.end())and *state != "") and counter_down !=0)
+				{
+					counter_down += 1;
+					++state;
+					EndShiftState = state;
+					ShiftBoofer.push_back(*state);
+					
+				}
+
+				else if ( (ShiftState and state == std::prev(out.end())) or (ShiftState and counter_down == 0))
+				{
+					counter_down += 1;
+					EndShiftState = state;
+					ShiftBoofer.push_back(*state);
+				}
+			}
+			else if (inp == "Up")
+			{
+				if (state != out.begin()and !ShiftState)
+				{
+					--state;
+				}
+				else if (ShiftState and state != out.begin())
+				{
+					counter_up += 1;
+					ShiftBoofer.push_back(*StartShiftState);
+					EndShiftState = state;
+					--state;
+
+				}
+			}
+
+			//CTRL+X
+
+
+			else if (inp == "Ctrl+X")
+			{
+				if ((!ShiftState) and (!state->empty()) and (state != std::prev(out.end())))
+				{
+					copy = *state;
+					state = out.erase(state);
+					ShiftBoofer = {};
+				}
+				else if ((!ShiftState) and (!state->empty()) and (state != std::prev(out.end())))
+				{
+					copy = *state;
+					*state = "";
+					ShiftBoofer = {};
+				}
+				if ((counter_down == counter_up) and counter_down != 0)
+				{
+					Special = 1;
+				}
+
+
+				if ((ShiftState) and (ShiftBoofer.size() > 0) and (!Special))
+				{
+					if ((std::distance(StartShiftState, EndShiftState) >= 0) and (std::next(EndShiftState) != out.end()))
+					{
+						out.erase(StartShiftState, std::next(EndShiftState));
+					}
+					else if ((std::distance(StartShiftState, EndShiftState) > 0) and (std::next(EndShiftState) == out.end()))
+					{
+						out.erase(StartShiftState, EndShiftState);
+					}
+					else if ((std::distance(StartShiftState, EndShiftState)) < 0)
+					{
+						out.erase(EndShiftState, StartShiftState);
+					}
+					ShiftState = 0;
+					PasteBoofer = ShiftBoofer;
+					ShiftBoofer = {};
+				}
+				else if ((ShiftState) and (ShiftBoofer.size() > 0) and (Special))
+				{
+					if ((std::distance(StartShiftState, EndShiftState) >= 0) and (std::next(EndShiftState) != out.end()))
+					{
+						out.erase(StartShiftState, std::next(EndShiftState));
+					}
+					else if ((std::distance(StartShiftState, EndShiftState) > 0) and (std::next(EndShiftState) == out.end()))
+					{
+						out.erase(StartShiftState, EndShiftState);
+					}
+					else if ((std::distance(StartShiftState, EndShiftState)) < 0)
+					{
+						out.erase(EndShiftState, StartShiftState);
+					}
+					ShiftState = 0;
+					PasteBoofer.push_back(copy);
+				}
+				StartShiftState = state;
+				counter_down = 0;
+				counter_up = 0;
+
+			}
+			//CTRL+V
+			else if ((inp == "Ctrl+V"))
+			{
+				if (!ShiftState and !copy.empty())
+				{
+					if (state != out.begin())
+					{
+						out.insert(state, copy);
+					}
+					else if (state == out.begin() and state->empty())
+					{
+						*state = copy;
+					}
+					else if (state == out.begin() and !state->empty())
+					{
+						out.insert(state, copy);
+					}
+
+				}
+
+				else if (ShiftState and (ShiftBoofer.size() > 0))
+				{
+					//ВЫРЕЗАТЬ И ВСТАВИТЬ НА ЭТИ ПОЗИЦИИ
+					out.insert(StartShiftState, PasteBoofer.begin(), PasteBoofer.end());
+					if (std::distance(StartShiftState, EndShiftState) > 0)
+					{
+						out.erase(StartShiftState, std::next(EndShiftState));
+					}
+
+					else if ((std::distance(StartShiftState, EndShiftState)) < 0)
+					{
+						out.erase(EndShiftState, StartShiftState);
+					}
+					ShiftState = 0;
+					PasteBoofer = ShiftBoofer;
+					ShiftBoofer = {};
+				}
+			}
+		}
 	}
-	auto end = std :: prev(output.end());
-	std::cout << *end;
-	if (*end == "")
+	if (out.size() > 0)
 	{
-		output.pop_back();
+		for (auto iter = out.begin(); iter != std::prev(out.end()); ++iter)
+		{
+			std::cout << *iter << '\n';
+
+		}
 	}
-
-
-
-	for (auto it = output.begin(); it != output.end(); ++it)
+	else
 	{
-		std::cout << *it << '\n';
+		std::cout << '\n';
 	}
 
-
-	 
 }
