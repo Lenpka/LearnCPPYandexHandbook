3df203ec361eaa49b305c61b0d377655c0a20829
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-24 21:47:50 +0300

    'countIter'

---
+++ 3.3/AlgorithmSort/AlgorithmSort.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <list>
+template <typename Iter, typename Value>
+int coun(Iter first, Iter last,const Value& value) // iterator is easy to copy
+{
+	int ret = 0;
+	for (Iter i = first; i != last; ++i)
+	{
+		if (*i == value)
+		{
+			++ret;
+	}
+	}
+	return ret;
+
+}
+
+
+int main()
+{
+	std::list <int> test = { 1, 2, 3, 3, 3, 4, 5, 3, 1 };
+	std::cout << coun(test.begin(), test.end(), 3);
+
+
+}
+//int main()
+//{
+//	return 0;
+//}
