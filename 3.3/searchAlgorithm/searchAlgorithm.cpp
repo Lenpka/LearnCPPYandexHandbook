e80374515e6f137b3b72f907f42dbd639ff5df58
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-24 22:20:46 +0300

    'searchElement'

---
+++ 3.3/searchAlgorithm/searchAlgorithm.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <list>
+template <typename Iter, typename Value>
+int fin(Iter first, Iter end, Value value)
+{
+	int ret{ 0 };
+	while (first != end)
+	{
+	
+		if (*first == value)
+		{
+			
+			return ret;
+		}
+		else
+		{
+			ret += 1;
+		}
+		++first;
+	}
+	return ret;
+
+
+}
+
+int main()
+
+{
+	std::list<int> ls{ {1,2,3,4,5} };
+	std::cout << fin(ls.begin(), ls.end(),3);
+
+}
