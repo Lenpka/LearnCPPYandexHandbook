e6b690149c25e2ee3a1561d8151a32210aa6aabc
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-25 13:25:04 +0300

    'HELP'

--- 3.3/A/A.cpp
+++ 3.3/A/A.cpp
@@ -1,30 +1,49 @@
 ﻿#include <iostream>
 #include <algorithm>
-#include <vector>
-template <typename ContainerValue>
-void Duplicate(std::vector<ContainerValue>& into)
+#include <unordered_set>
+template <typename Iter>
+Iter Uniqu(Iter first, Iter last) 
 {
-	into.resize(into.size() * 2);
-	auto first = into.begin();
-	auto firstEnd = (into.end()) - into.size() / 2;
-	auto ended = (into.end()) - into.size() /2;
-	while (first != firstEnd)
+	using ValueType = typename std::iterator_traits <Iter> ::value_type;
+	if (first == last)
 	{
-		*ended = *first;
-		++first;
-		++ended;
+		return last;
 	}
+	
+	std::unordered_set<ValueType> templ;
+	//int lenCycle = static_cast<int>(std::distance(first, last));
+	Iter it = first;// Допустим 8
+	for (Iter i = first; i != last; ++i)
+	{
+		auto what = templ.insert(*i);
+		auto how = what.second;
+		if (how)
+		{
+			*it = std::move(*i);
+			++it;
 
-}
+		}
+		
+	}
+	return it;
 
+	//std
+	////arr = std::copy(first, last, std::next(last));	
+	//bool flag = false;
+	//while (first != last)
+	//{
+	//	int start = 1; int counter = 0;
+	//	for (int n = 1; *first == *(first + n); n++)
+	//	{
+	//		++counter;
+	//	}
+
+
+	//	++first;
+	//}
 
 int main()
 {
-	std::vector<int> test = {1, 2, 3};
-	Duplicate(test);
-	for (const auto& it : test)
-	{
-		std::cout << it << '\n';
-	}
-
+	std::vector <int> test {5, 3, 3, 5, 5, 2, 1, 9};
+	std::cout << *Uniqu(test.begin(), test.end());
 }
