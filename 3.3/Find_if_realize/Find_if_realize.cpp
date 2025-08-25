bf74b232864a09ee9fc1ef122ddd06434e9b9a8c
Lenpka <leanidkonstantinov@yandex.ru>
2025-08-25 06:45:26 +0300

    'FindIf'

---
+++ 3.3/Find_if_realize/Find_if_realize.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <set>
+template <typename Iter, typename Predicate>
+int fin_if(Iter begin, Iter end, Predicate func)
+{
+	int ret = 0;
+	while (begin != end)
+	{
+		if (func(*begin) == 1)
+		{
+			return ret;
+		}
+		++begin;
+		ret += 1;
+
+	}
+	return ret;
+}
+
+int main()
+{
+	std::set<int> test = { 1 , 2, 3, 4, 5 };
+	std::cout << fin_if(test.begin(), test.end(), [](int i)->bool {return i > 3; });
+}
